8163ce538d072dff81e290abc6f6e5b82e4e6f09
carsonadair <carsonadairphoto@gmail.com>
2023-03-11 14:44:43 -0500

    write index.js file with questions and function to write readme file

--- index.js
+++ index.js
@@ -1,15 +1,65 @@
 // Includes packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      message: 'What is your project?',
+      name: 'nameInput',
+    },
+    {
+      type: 'input',
+      message: 'What is your description?',
+      name: 'descriptionInput',
+    },
+    {
+        type: 'input',
+        message: 'How do you install your project?',
+        name: 'installInput',
+      },
+    {
+      type: 'list',
+      message: 'Select a license for your project.',
+      choices: [
+        'MIT License',
+        'IBM',
+        'Creative Common',
+        'Mozilla Public License 2.0',
+        ]
+    },
+    {
+      type: 'input',
+      message: 'How do you use your project?',
+      name: 'usageInput',
+    },
+    {
+      type: 'input',
+      message: 'Who contributed to this project?',
+      name: 'contribInput',
+    },
+    {
+        type: 'input',
+        message: 'Describe how to test your project.',
+        name: 'testInput',
+      },
+  ]
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+// Function to write README file and confirm
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+    err ? console.error(err): console.log('README.md has been successfully generated.')
+    );
+}
+// function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function(userInputs){
+        writeToFile('README.md', generateMarkdown(userInputs));
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,19 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
+function renderLicenseBadge(license) {  
+  switch(license){
+   case 'MIT License':
+     return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'IBM':
+     return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+   case 'Creative Common':
+    return '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)'
+   case 'Mozilla Public License 2.0':
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+   case 'None':
+    return '![No License](https://img.shields.io/badge/No_License-0298c3.svg)';
+
+}}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
